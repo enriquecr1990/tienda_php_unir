4c48c3d01e459aa9e755aa94a79b24f253278f76
Enrique Corona <enrique_cr1990@hotmail.com>
2021-02-02 09:11:02 -0600

    actualizacion de la configuracion de la BD para la tienda en linea alojada en el hosting

--- model/ConfigDB.php
+++ model/ConfigDB.php
@@ -8,6 +8,13 @@ class ConfigDB {
         // switch de $_SERVER['SERVER_NAME'] para cargar la configuracion de la
         // BD para que funcione en local o en la nube
         switch ($_SERVER['SERVER_NAME']){
+            case 'tienda-php.enriquecr-mx.info':
+                $db_config['host'] = 'localhost';
+                $db_config['port'] = '3306';
+                $db_config['user'] = 'u471544287_ecr_unir';
+                $db_config['password'] = 'Pa$$word1234';
+                $db_config['data_base'] = 'u471544287_mi_tienda_unir';
+                break;
             default:
                 $db_config['host'] = 'localhost';
                 $db_config['port'] = '3306';
