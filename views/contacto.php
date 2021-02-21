2d7f3709b17a6934b2ea66eaed50edfa0cc9c192
Enrique Corona <enrique_cr1990@hotmail.com>
2021-02-21 02:10:19 -0600

    actualizacion por el w3c validator

--- views/contacto.php
+++ views/contacto.php
@@ -12,7 +12,7 @@
                         <strong>Contacto: </strong> 246 757 5099<br>
                         <strong>Domicilio: </strong> Privada villa de las playas S/N, Acuitlapilco, Tlaxcala, CP: 90110
                     </p>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d941.4654188956775!2d-98.23977745538544!3d19.28838064105156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cfd9629ea94d3f%3A0x25ddc8759c3ef10d!2sTaqueria%20los%20Altos%20de%20Jalisco!5e0!3m2!1ses-419!2smx!4v1612077063068!5m2!1ses-419!2smx" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d941.4654188956775!2d-98.23977745538544!3d19.28838064105156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cfd9629ea94d3f%3A0x25ddc8759c3ef10d!2sTaqueria%20los%20Altos%20de%20Jalisco!5e0!3m2!1ses-419!2smx!4v1612077063068!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                 </div>
             </div>
         </div>
